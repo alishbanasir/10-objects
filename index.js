27c4a588618055f766b8dc7c2ae8179e966379c5
Alishba Nasir <165299398+alishbanasir@users.noreply.github.com>
2024-10-02 19:55:23 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,114 @@
+var student1 = {
+    firstName:"Alishba",
+    lastName:"Nasir",
+    age:20,
+    qualification:"Enter",
+    Email:"alishbanas54@gmail.com",
+    PhoneNumber:+921234587
+};
+  var student2 = {
+        firstName:"Faiza",
+        lastName:"Khan",
+        age:23,
+        qualification:"enter",
+        Email:"fiz12h14@gmail.com",
+        PhoneNumber:+9212332245
+};
+var student3 = {
+    firstName:"Dua",
+    lastName:"Muneer",
+    age:22,
+    qualification:"matric",
+    Email:"dua22224@gmail.com",
+    PhoneNumber:+92122564345
+};
+var student4 = {
+    firstName:"Nimra",
+    lastName:"Inam",
+    age:25,
+    qualification:"bacholars",
+    Email:"nim77324@gmail.com",
+    PhoneNumber:+921234235
+};
+var student5 = {
+    firstName:"Laiba",
+    lastName:"Asif",
+    age:26,
+    qualification:"graduate",
+    Email:"dish82812@gmail.com",
+    PhoneNumber:+9212343345
+};
+var student6 = {
+    firstName:"Abiha",
+    lastName:"Khan",
+    age:23,
+    qualification:"diploma",
+    Email:"aliz7367@gmail.com",
+    PhoneNumber:+9212342395,
+};
+var student7 = {
+    firstName:"dania",
+    lastName:"Aslam",
+    age:21,
+    qualification:"enter",
+    Email:"dua22224@gmail.com",
+    PhoneNumber:+9212345,
+};
+var student8 = {
+    firstName:"Manahil",
+    lastName:"Ali",
+    age:22,
+    qualification:"1st year",
+    Email:"man732@gmail.com",
+    PhoneNumber:+92182832345,
+};
+var student9 = {
+    firstName:"Maryam",
+    lastName:"Khan",
+    age:17,
+    qualification:"matric",
+    Email:"mar2738@gmail.com",
+    PhoneNumber:+9268712345,
+};
+var student10 = {
+    firstName:"Raima",
+    lastName:"Islam",
+    age:20,
+    qualification:"enter",
+    Email:"bush2387@gmail.com",
+    PhoneNumber:+9217362345,
+};
+
+
+document.getElementById("stud1").innerHTML=
+student1.firstName + "  " + student1.lastName +"  a motivated " + student1.age +" years old , qualification in Enter, she is eager to take on new challenges and grow both personally and professionally. You can reach her via email at " + student1.Email + " or contact her by phone at " + student1.PhoneNumber ;
+
+document.getElementById("stud2").innerHTML=
+student2.firstName + "  " + student2.lastName +"  a vibrant " + student2.age +" years old , is dedicated to continuous learning and self-improvement. With her qualification in enter, she is prepared to embrace new opportunities and challenges. Fiza can be contacted via email at " + student2.Email + " or by phone at +" + student2.PhoneNumber;
+
+document.getElementById("stud3").innerHTML=
+student3.firstName + "  " + student3.lastName +" a driven " + student3.age +" years old , is passionate about expanding her knowledge and taking on new challenges. With a qualification in matric, she is well-prepared for future opportunities. Dua is easily reachable via email at " + student3.Email + " or by phone at +" + student3.PhoneNumber;
+
+document.getElementById("stud4").innerHTML=
+student4.firstName + "  " + student4.lastName +"  a  " + student4.age +" years old with a Bachelor's degree, is driven by a desire for continuous growth and professional development. Ready to take on new challenges, she is open to exploring exciting opportunities. Nimra can be contacted via email at " + student3.Email + " or by phone at +" + student3.PhoneNumber;
+
+document.getElementById("stud5").innerHTML=
+student5.firstName + "  " + student5.lastName +"  a  " + student5.age +" years old graduate, is enthusiastic about pursuing new opportunities and expanding her skills. With her educational background, she is well-prepared for challenges ahead. You can reach Darisha via email at " + student3.Email + " or by phone at +" + student3.PhoneNumber;
+
+document.getElementById("stud6").innerHTML=
+student6.firstName + "  " + student6.lastName +"  a  " + student6.age +" years old with a diploma, is passionate about honing her skills and pursuing exciting opportunities. With her background and enthusiasm, she is ready to take on new challenges. You can contact Aliza via email at " + student3.Email + " or by phone at +" + student3.PhoneNumber;
+
+document.getElementById("stud7").innerHTML=
+student7.firstName + "  " + student7.lastName +"  a dynamic  " + student7.age +" years old is passionate about personal development and ready to embrace new challenges with her qualification in enter. With a keen interest in exploring opportunities, she is enthusiastic about her future. You can reach Dania via email at " + student3.Email + " or by phone at +" + student3.PhoneNumber;
+
+document.getElementById("stud8").innerHTML=
+student8.firstName + "  " + student8.lastName +"  a dedicated   " + student8.age +" years old in her first year of studies, is passionate about learning and exploring new horizons. With her enthusiasm and commitment, she is ready to take on exciting challenges ahead. You can contact Mantasha via email at " + student3.Email + " or by phone at +" + student3.PhoneNumber;
+
+document.getElementById("stud9").innerHTML=
+student9.firstName + "  " + student9.lastName +"  a brigh  " + student9.age +" years old with a matric qualification, is at the beginning of her educational journey, eager to explore new possibilities. Her enthusiasm and determination to learn set her apart as she looks forward to future challenges. You can contact Maryam via email at " + student3.Email + " or by phone at +" + student3.PhoneNumber;
+
+document.getElementById("stud10").innerHTML=
+student10.firstName + "  " + student10.lastName +"  a motivated  " + student10.age +" years old is eager to make her mark as she embarks on her journey with a qualification in enter. Her enthusiasm for learning and growth drives her to seek new opportunities and challenges. You can reach Bushra via email at " + student3.Email + " or by phone at +" + student3.PhoneNumber;
+
+
+
